a9ad9ef6f32129a44b2a865434457b3a6b8e6534
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-06-12 11:14:47 +0700

    Rendering subject display

--- button/pvt.py
+++ button/pvt.py
@@ -11,3 +11,7 @@ from flask import (
 
 bp = Blueprint("pvt", __name__, url_prefix="/pvt")
 
+
+@bp.route("/display", methods=("GET"))
+def display_screen():
+    return render_template("subject_display.html")
