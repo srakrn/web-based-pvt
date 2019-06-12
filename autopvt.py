0865ef09dcbb5db415b14958ad456b59520f87f3
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-06-12 15:32:56 +0700

    AutoPVT for testing

--- autopvt.py
+++ autopvt.py
@@ -1,13 +1,33 @@
 import socketio
 import random
 import time
+import datetime
 
 s = socketio.Client()
-
 s.connect("http://localhost:8888")
+received = True
+before_time = datetime.datetime.now()
 
+f = open("pvtlog.txt", "a")
+
+
+@s.on("screen")
+def after(data):
+    global received
+    if data["action"] == "tapped":
+        print("Button received")
+        after_time = datetime.datetime.now()
+        f.write("{}\n".format(after_time - before_time))
+        received = True
+
+
+time.sleep(10)
 while True:
-    t = random.triangular(3, 7)
-    print("Emit")
+    period = random.triangular(3, 7)
+    before_time = datetime.datetime.now()
     s.emit("control", {"to": "pvt", "action": "start", "duration": 500})
-    time.sleep(t)
+    print("Transmitted")
+    received = False
+    while not received:
+        pass
+    time.sleep(period)
